63618b2569b5a3f74061897952be88931caceac6
deibo <deib0@foxmail.com>
2022-08-09 18:09:31 +0800

    原生React实现demo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import React from "react"
+import { useRef,useState } from "react"
+
 function App(){
+    const [n,setN]=useState(0)
+    const selectRef = useRef(null)
+    const increase=()=>{
+        setN(selectRef.current.value-0+n)
+    }
     return (
         <>
-        App
+        <div>{n}</div>
+        <select ref={selectRef}>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+        </select>
+        <button onClick={()=>increase()}>increase</button>&nbsp;
+        <button>decrease</button>&nbsp;
+        <button>increase if odd</button>&nbsp;
+        <button>increase async</button>&nbsp;
         </>
     )
 }
